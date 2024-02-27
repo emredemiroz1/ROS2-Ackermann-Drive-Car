d6eb2ad6940f1ac1486059798cf4483caba50482
Emre-ino <b200104067@subu.edu.tr>
2024-02-27 13:40:10 +0300

    display.xml update

---
+++ src/build/ack_robot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/emred/ackerman_ws/src/ack_robot_bringup
+# Build directory: /home/emred/ackerman_ws/src/build/ack_robot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
